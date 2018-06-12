05a45fc0e4ba9d1ef9160380c137cc3ee1868b7d
Eric Trombly <etrombly@yahoo.com>
2018-06-12 09:09:12 -0500

    Change example image and remove chrono dep

--- examples/timestamp.rs
+++ examples/timestamp.rs
@@ -1,20 +1,16 @@
-// example image from http://www.gaia-gis.it/spatialite-2.3.1/resources.html
+// example image from https://github.com/drewnoakes/metadata-extractor-images
 
 extern crate rexiv2;
-extern crate chrono;
 
 use rexiv2::Metadata;
-use chrono::NaiveDateTime;
 
 fn main() {
-    if let Ok(meta) = Metadata::new_from_path("examples/DSCN0010.jpg") {
+    if let Ok(meta) = Metadata::new_from_path("examples/FujiFilm FinePixS1Pro (1).jpg") {
         if let Ok(time) = meta.get_tag_string("Exif.Image.DateTime") {
-            if let Ok(timestamp) = NaiveDateTime::parse_from_str(&time, "%Y:%m:%d %H:%M:%S") {
-                println!("Time: {:?}", timestamp);
-            }
+            println!("Time: {:?}", time);
         }
         if meta.set_tag_string("Exif.Image.DateTime", "2008:11:01 21:15:07").is_ok() {
-            meta.save_to_file("examples/DSCN0010.jpg").expect("Couldn't save metadata to file");
+            meta.save_to_file("examples/FujiFilm FinePixS1Pro (1).jpg").expect("Couldn't save metadata to file");
         }
     }
 }
