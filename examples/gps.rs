766b60cadede67c8d52fba882d86628676adb720
Jonas Hagen <jonas.hagen@qrst.ch>
2021-07-16 18:56:29 +0200

    Change GPS example to get latitude, longitude, altitude separately.

--- examples/gps.rs
+++ examples/gps.rs
@@ -4,8 +4,20 @@ use rexiv2::Metadata;
 
 fn main() {
     if let Ok(meta) = Metadata::new_from_path("examples/example.jpg") {
-        if let Some(location) = meta.get_gps_info() {
-            println!("Location: {:?}", location);
+        if let Some(lat) = meta.get_gps_latitude() {
+            println!("Latitude: {:?}", lat);
+        } else {
+            println!("No latitude found.");
+        }
+        if let Some(lon) = meta.get_gps_longitude() {
+            println!("Longitude: {:?}", lon);
+        } else {
+            println!("No longitude found.");
+        }
+        if let Some(alt) = meta.get_gps_altitude() {
+            println!("Altitude: {:?}", alt);
+        } else {
+            println!("No altitude found.");
         }
     }
 }
